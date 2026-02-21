c3f7ea213d55b456c357ebebc28461b8e51447df
Karol Kolański <kkolanski.edu@gmail.com>
2026-02-21 15:06:23 +0100

    First send keys ever

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from itertools import dropwhile
+
 from selenium.webdriver import Chrome
 from time import sleep
 from selenium.webdriver.common.by import By
+
+# Dane testowe
+DATA_EMAIL = "kdhfsjdh@spam.la"
 
 # Stworzenie instancji klasy Chrome
 # (to otworzy przeglądarkę)
@@ -10,13 +15,14 @@ driver.get("https://automationpractice.techwithjatin.com/")
 
 # Maksymalizacja okna
 driver.maximize_window()
-
 # Znajdz element Sign in
 sign_in_a = driver.find_element(By.PARTIAL_LINK_TEXT, "Sign in")
 # Kliknij w odnaleziony element
 sign_in_a.click()
 
-print(type(sign_in_a))
+# Wpisz email
+driver.find_element(By.ID, "email_create").send_keys(DATA_EMAIL)
+
 sleep(5)
 driver.quit()
 
